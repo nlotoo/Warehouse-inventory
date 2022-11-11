917adcac847b11a30e4127d8b88b1dab5ed10a03
Yordan Nikolov <jnikolow93@gmail.com>
2022-11-11 07:31:53 +0000

    add delete button logic

--- php-task/index.php
+++ php-task/index.php
@@ -1,3 +1,33 @@
+<?php
+
+$dbname   = 'db-php-task';
+$username = 'root';
+$password = '';
+
+
+$pdo = new PDO("mysql:host=localhost;dbname=$dbname", $username,  $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+echo $_SERVER['REQUEST_METHOD'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $product = $_POST["product-name"];
+    $price = $_POST["price"];
+    $weight = $_POST["weight"];
+    $date = date('Y-m-d H:i:s');
+
+    $statement = $pdo->prepare("INSERT INTO product (name, price,weight, date)
+                                VALUES (:name,:price, :weight, :date)");
+    $statement->bindValue(':name', $product);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':weight', $weight);
+    $statement->bindValue(':date', $date);
+    $statement->execute();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,40 +40,25 @@
 </head>
 
 <body>
-    <?php
-    include './conection.php'
 
-    ?>
 
     <!-- 1/ Страница/форма, чрез която се добавя продукт (име, тегло, стойност в лв., дата на добавяне)
  -->
 
 
-    <form action="d-colector.php" method="post" class="form-container">
+    <form action="" method="post" class="form-container">
         <label>Name</label>
         <input id="name" type="text" name="product-name" placeholder="name of product">
         <label>Weight</label>
         <input id="weight" type="text" name="weight" placeholder="weight">
         <label>Price</label>
         <input id="price" type="text" name="price" placeholder="price">
-        <label>Date</label>
-        <input id="date" type="date" name="date">
+
 
         <input type="submit" name="btn-act" class="add-button" value="Submit">
     </form>
 
 
-
-    <!-- 2/ Страница, която показва всички продукти със странициране и филтри.
-     Филтрите да бъдат по име и дата на добавяне. Да има възможност за изтриване на продуктите.
- -->
-    <div class="products-container">
-        <h1>Products</h1>
-        <ul>
-            <li>product1</li>
-        </ul>
-
-    </div>
 
 
 
--- php-task/second-page/index.php
+++ php-task/second-page/index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/styles.css">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <title>Document</title>
-</head>
 <?php
 $dbname   = 'db-php-task';
 $username = 'root';
@@ -26,46 +13,56 @@ $statement->execute();
 
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<pre>';
-var_dump($products);
-echo '</pre>';
-
-
-
-
-
-
+// echo '<pre>';
+// var_dump($products);
+// echo '</pre>';
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
 
 <body>
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Name of product</th>
+                <th scope="col">Price</th>
+                <th scope="col">Weight</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php foreach ($products as $i => $p) : ?>
+
+                <tr>
+                    <th scope="row"><?php echo $i + 1 ?></th>
+                    <td><?php echo $p['name']; ?></td>
+                    <td><?php echo $p['price']; ?> lv.</td>
+                    <td><?php echo $p['weight']; ?> kg.</td>
+                    <td><?php echo $p['date']; ?></td>
+                    <td>
+                        <?php echo $p['id_product'] ?>
+                        <form method="post" action="delete.php">
+                            <input type="hidden" name="id" value="<?php echo $p['id_product'] ?>">
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+
+            <?php endforeach; ?>
         </tbody>
     </table>
 </body>
